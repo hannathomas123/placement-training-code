e717e576c167d4e30276b9c10cd9d9ee05ae3763
hannathomas123 <hannathomas699@gmail.com>
2025-08-03 19:27:16 +0530

    Create reverse_array.c

---
+++ reverse_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+    int n;
+    printf("Enter number of elements: ");
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid size.\n");
+        return 1;
+    }
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array in place
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    printf("Reversed array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
