a17619dda0854fe5b7ebe84f93f6d22ce4520bb7
hannathomas123 <hannathomas699@gmail.com>
2025-08-03 19:12:31 +0530

    Create digit_to_words.c

---
+++ digit_to_words.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+// Function to print word for each digit
+void printDigit(char c) {
+    switch (c) {
+        case '0': printf("zero"); break;
+        case '1': printf("one"); break;
+        case '2': printf("two"); break;
+        case '3': printf("three"); break;
+        case '4': printf("four"); break;
+        case '5': printf("five"); break;
+        case '6': printf("six"); break;
+        case '7': printf("seven"); break;
+        case '8': printf("eight"); break;
+        case '9': printf("nine"); break;
+        default: printf(""); break;
+    }
+}
+
+int main() {
+    char
